19c869c1ba3a20906f49c555a524fced53c70540
Andrew Randolph <andrew.randolph@libertymutual.com>
2017-11-14 08:37:23 -0500

    Mapper updates for the mapping pins

--- src/containers/Mapper.js
+++ src/containers/Mapper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Listing from '../components/Listing';
+import PropTypes from 'prop-types';
 import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
@@ -7,7 +8,6 @@ export class MapContainer extends React.Component {
     super(props);
 
     this.state = {
-      places: [],
       placeIds: [],
       showingInfoWindow: false,
       activeMarker: {},
@@ -16,13 +16,21 @@ export class MapContainer extends React.Component {
 
     this.fetchPlaces = this.fetchPlaces.bind(this);
     this.markerClick = this.markerClick.bind(this);
-    //this.listPlaces = this.listPlaces.bind(this);
   }
 
   fetchPlaces(mapProps, map) {
     const {google, initialCenter: { lat, lng }} = mapProps;
+    const {agencies} = this.props;
+    const resultIds = [];
     const service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({location: {lat, lng}, radius: 3000}, (results) => results.map(result => service.getDetails({placeId: result.place_id}, (result) => this.setState({placeIds: this.state.placeIds.concat(result)}))));
+    const promise = new Promise((resolve, reject) => {
+      agencies.map(agency => service.nearbySearch({location: {lat, lng}, radius: 16000}, (results) => resolve(results)));
+    });
+    promise.then(results => {
+      results.map(result => resultIds.push(result.place_id));
+      console.log(resultIds);
+    });
+    // agencies.map(agency => service.nearbySearch({location: {lat, lng}, radius: 100000, keyword:agency.title}, (results) => results.map(result => service.getDetails({placeId: result.place_id}, (result) => this.setState({placeIds: this.state.placeIds.concat(result)})))));
   }
 
   markerClick(props, marker, e) {
@@ -34,7 +42,6 @@ export class MapContainer extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     const style = {
       width: '600px',
       height: '600px'
@@ -50,14 +57,16 @@ export class MapContainer extends React.Component {
           lng: -88.081807
         }}
       >
-      {this.state.places.map((place, index) => {
+      {this.state.placeIds.map((place, index) => {
         return (
           <Marker
             key={index}
             onClick={this.markerClick}
-            title={place.name}
-            name={place.name}
-            position={{lat: place.geometry.location.lat(), lng: place.geometry.location.lng()}}
+            title={place && place.name}
+            number={place && place.international_phone_number}
+            address={place && place.formatted_address}
+            name={place && place.name}
+            position={{lat: place && place.geometry.location.lat(), lng: place && place.geometry.location.lng()}}
            />
         );
       })}
@@ -66,13 +75,20 @@ export class MapContainer extends React.Component {
         marker={this.state.activeMarker}
         visible={this.state.showingInfoWindow}>
         <div>
-          <h1>{this.state.selectedPlace.name}</h1>
+          <h4>{this.state.selectedPlace.name}</h4>
+          <h4><a href={this.state.selectedPlace.number}>{this.state.selectedPlace.number}</a></h4>
+          <h4>{this.state.selectedPlace.address}</h4>
         </div>
       </InfoWindow>
       </Map>
     );
   }
 }
+
+MapContainer.propTypes = {
+  google: PropTypes.object.isRequired,
+  agencies: PropTypes.array
+};
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo')
