3e085d518e46122e2e9d9305c038a7a83b13b896
Andrew Randolph <andrew.randolph@libertymutual.com>
2017-12-06 14:18:02 -0500

    Code cleanup of burger menu and proptypes

--- src/components/burgerMenu/BurgerMenu.js
+++ src/components/burgerMenu/BurgerMenu.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import BurgerMenuChild from './BurgerMenuChild';
-import { Link, IndexLink } from 'react-router';
 
-class BurgerMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuIsEnabled: this.props.menuIsEnabled
-    };
-  }
-  handleClick() {
-    const menuNav = document.querySelector('nav');
-    this.setState({
-      menuIsEnabled: !this.state.menuIsEnabled
-    }, menuNav.classList.toggle("navIsExpanded"));
-  }
-  render() {
-    return (
-      <BurgerMenuChild className = {this.state.menuIsEnabled ? "burgerMenu menuIsExpanded" : "burgerMenu"}  onClick = {() => this.handleClick()}>Menu <div className="tripleBurger1"></div> <div className="tripleBurger2"></div> <div className="tripleBurger3"></div> </BurgerMenuChild>
-    );
-  }
-}
-
-BurgerMenu.propTypes = {
-  menuIsEnabled: false
+const BurgerMenu = ({menuIsEnabled, onClick}) => {
+  return (
+    <div className = {menuIsEnabled ? "burgerMenu menuIsExpanded" : "burgerMenu"}  onClick={onClick}>Menu
+      <div className="tripleBurger1"></div>
+      <div className="tripleBurger2"></div>
+      <div className="tripleBurger3"></div>
+    </div>
+  );
 };
 
-BurgerMenu.defaultProps = {
-  menuIsEnabled: false
+BurgerMenu.propTypes = {
+  menuIsEnabled: PropTypes.boolean,
+  onClick: PropTypes.function
 };
 
 export default BurgerMenu;
