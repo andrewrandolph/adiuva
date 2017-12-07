60dc3c77d0d4d8dfc7a3310f207512ed8387c8c7
COREY JORDAN-RUTLEDGE <Corey.Jordan-Rutledge@libertymutual.com>
2017-12-07 15:56:26 -0500

    Cleaned this file up , made class a variable, etc.

--- src/components/about/aboutApp.js
+++ src/components/about/aboutApp.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as mapActions from '../../actions/mapActions';
 
-class aboutAdiuvaApp extends React.Component {
-
-  render() {
+const aboutAdiuvaApp = () => {
     return (
       <div className="aboutAdiuva">
         About Adiuva! - Coming soon...
       </div>
-    );
-  }
-}
+  );
+};
 
 export default aboutAdiuvaApp;
